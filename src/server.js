c2c35b79f4a6391594ca2e071b052265d47b9855
koleso8 <dev.koleso@gmail.com>
2024-11-08 14:45:49 +0200

    finish add swagger docs

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -19,6 +20,8 @@ export const startServer = () => {
   app.use(cors());
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
+
   // app.use(
   //   pinoHttp({
   //     transport: {
@@ -40,6 +43,7 @@ export const startServer = () => {
   app.use(errorHandler);
 
   app.listen(PORT, () => {
-    console.log(`server is run on ${PORT}`);
+    console.log(`server is run on http://localhost:${PORT}/`);
+    console.log(`API-docs http://localhost:${PORT}/api-docs`);
   });
 };
